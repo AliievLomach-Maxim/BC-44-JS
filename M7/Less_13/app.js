a8290a1aefe0379af1a04d553fa72dcc05750934
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-01-13 19:26:01 +0200

    add less-14

---
+++ M7/Less_13/app.js
@@ -0,0 +1,53 @@
+// ⁡⁣⁣⁢​‌‍‌​‌‌‍‍ ‍Модуль 𝟳. Заняття 𝟭𝟯. 𝗕𝘂𝗯𝗯𝗹𝗶𝗻𝗴
+
+// const refs = {
+// 	card: document.querySelector('.card'),
+// 	btn: document.querySelector('.btn'),
+// 	main: document.querySelector('.main'),
+// }
+
+// refs.main.addEventListener('click', handleClickCard)
+// // refs.btn.addEventListener('click', handleClickBtn)
+
+// function handleClickCard(event) {
+// 	if (event.target.nodeName !== 'BUTTON') {
+// 		alert('Lorem')
+// 	} else if (event.target.tagName === 'BUTTON') {
+// 		console.log('object :>> ')
+// 	}
+// }
+
+// function handleClickBtn(event) {
+// 	event.stopPropagation()
+// 	console.log('click BTN :>> ')
+// }
+// ===============
+
+// const main = document.querySelector('.main')
+
+// main.addEventListener('click', handleClickCard)
+
+// function handleClickCard(ev) {
+// 	console.log('ev.target :>> ', ev.target)
+// 	console.log('ev.currentTarget :>> ', ev.currentTarget)
+// 	if (ev.target !== ev.currentTarget && ev.target.nodeName !== 'BUTTON') {
+// 		alert('Lorem')
+// 	} else if (ev.target.nodeName === 'BUTTON') {
+// 		console.log('object :>> ')
+// 	}
+// }
+
+// const createBtn = document.querySelector('.createBtn')
+// createBtn.addEventListener('click', () => {
+// 	const header = document.createElement('h1')
+// 	header.textContent = 'Less 13'
+// 	main.insertAdjacentElement('afterbegin', header)
+// })
+
+// createBtn.onclick = () => {
+// 	const header = document.createElement('h1')
+// 	header.textContent = 'Less 13'
+// 	main.insertAdjacentElement('afterbegin', header)
+// }
+
+// const main = document.querySelector('#idName>#images>img.img2')
