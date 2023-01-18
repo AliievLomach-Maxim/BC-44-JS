463faa5f05ab529d64176be647d59dff873a0062
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-01-18 14:03:08 +0200

    add less-15

---
+++ M8/less-15/src/index.js
@@ -0,0 +1,6 @@
+import './style.css'
+import getName, { number, number2 } from './scripts/app'
+// import * as importModuleFromApp from './scripts/app'
+
+getName('Alex')
+console.log('number', number)
